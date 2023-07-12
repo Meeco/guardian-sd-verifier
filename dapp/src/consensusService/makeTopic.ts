b0635a64d51ca395e57d671ceb19163c313ee96a
Wasin Nimlaor <wasin.nimlaor@gmail.com>
2023-07-12 14:52:28 +0930

    create makeTopic's tests

--- dapp/src/consensusService/makeTopic.ts
+++ dapp/src/consensusService/makeTopic.ts
@@ -1,18 +1,18 @@
 import { Client, TopicCreateTransaction } from "@hashgraph/sdk";
 
-const makeTopic = async (hederaClient: Client) => {
+const makeTopic = async (client: Client) => {
   try {
     // Create a new topic
-    let txResponse = await new TopicCreateTransaction().execute(hederaClient);
+    let txResponse = await new TopicCreateTransaction().execute(client);
     // Grab the newly generated topic ID
-    let receipt = await txResponse.getReceipt(hederaClient);
+    let receipt = await txResponse.getReceipt(client);
     let topicId = receipt.topicId;
     console.log(`Your topic ID is: ${topicId}`);
     // Wait 5 seconds between consensus topic creation and subscription creation
     await new Promise((resolve) => setTimeout(resolve, 5000));
     return topicId;
   } catch (error) {
-    console.log({ error });
+    console.log('Make topic failed', error);
   }
 };
 
