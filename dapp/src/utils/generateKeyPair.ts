c3475101334b1f002f52de204f9723deba110283
Sunny <wnimlaor@nextfaze.com>
2023-07-19 11:22:22 +0930

    Update generateKeyPair util's logic

--- dapp/src/utils/generateKeyPair.ts
+++ dapp/src/utils/generateKeyPair.ts
@@ -1,15 +1,27 @@
 import { Ed25519VerificationKey2018 } from "@digitalbazaar/ed25519-verification-key-2018";
-import { Ed25519Signature2018 } from "@digitalbazaar/ed25519-signature-2018";
+import bs58 from "bs58";
 
-export const generateKeyPair = async (id: string) => {
-  const keyPair = await Ed25519VerificationKey2018.generate({
-    controller: id,
-  });
-  return keyPair;
-};
+export const generateKeyPair = async ({
+  credentialSubject,
+  publicKeyHex,
+  privateKeyHex,
+}: {
+  credentialSubject: any;
+  publicKeyHex: string;
+  privateKeyHex: string;
+}) => {
+  const publicKeyBase58 = bs58.encode(Buffer.from(publicKeyHex, "hex"));
+  const privateKeyBase58 = bs58.encode(Buffer.from(privateKeyHex, "hex"));
 
-export const getSuite = (keyPair: any) => {
-  return new Ed25519Signature2018({
-    key: keyPair,
+  const { id, controller } = credentialSubject;
+
+  const key = await Ed25519VerificationKey2018.from({
+    id,
+    type: "Ed25519VerificationKey2018",
+    controller,
+    publicKeyBase58,
+    privateKeyBase58,
   });
+
+  return key;
 };
