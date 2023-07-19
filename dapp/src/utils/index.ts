cd678fee530568b16b7feb565471c3a79cd35f45
Sunny <wnimlaor@nextfaze.com>
2023-07-19 13:39:39 +0930

    create fetchIPFSFile util

--- dapp/src/utils/index.ts
+++ dapp/src/utils/index.ts
@@ -1,4 +1,5 @@
 export { documentLoader } from "./documentLoader";
+export * from "./fetchIPFSFile";
 export { generateKeyPair } from "./generateKeyPair";
 export * from "./getPublicKeyHexFromJwk";
 export { default as pollRequest } from "./pollRequest";
