485ac8d72ad12c92aeabb3f61de689b34398e7e7
Wasin Nimlaor <wasin.nimlaor@gmail.com>
2023-07-20 14:04:46 +0930

    [WIP] work around: query file contents by hardcoded account

--- dapp/src/fileService/getFileContents.ts
+++ dapp/src/fileService/getFileContents.ts
@@ -1,5 +1,5 @@
 import { BladeSigner } from "@bladelabs/blade-web3.js";
-import { FileContentsQuery } from "@hashgraph/sdk";
+import { Client, FileContentsQuery, Hbar } from "@hashgraph/sdk";
 import { Buffer } from "buffer";
 
 const readFileAsJson = (text: string) => {
@@ -10,12 +10,27 @@ const readFileAsText = async (contents: Uint8Array) => {
   return Buffer.from(contents).toString("utf-8");
 };
 
+// TODO: execute FileContentsQuery with signer instead of Hedera client when the executeWithSigner issue is resolved'
+const createHederaClient = () => {
+  const client = Client.forTestnet();
+  client
+    .setOperator(
+      process.env.REACT_APP_HEDERA_ACCOUNT_ID || "",
+      process.env.REACT_APP_HEDERA_PRIVATE_KEY || ""
+    )
+    .setDefaultMaxTransactionFee(new Hbar(1))
+    .setMaxQueryPayment(new Hbar(1));
+  return client;
+};
+
 const getFileContents = async (signer: BladeSigner, fileId: string) => {
   try {
     //Create the query
-    const query = new FileContentsQuery().setFileId(fileId).setMaxAttempts(10);
-    //Sign with client operator private key and submit the query to a Hedera network
-    const contents = await query.executeWithSigner(signer);
+    const query = new FileContentsQuery().setFileId(fileId);
+    const client = createHederaClient();
+    // TODO: execute FileContentsQuery with signer instead of Hedera client when the executeWithSigner issue is resolved'
+    const contents = await query.execute(client);
+    // const contents = await query.executeWithSigner(signer);
     return readFileAsJson(await readFileAsText(contents));
   } catch (error) {
     console.log("Get file contents failed", error);
