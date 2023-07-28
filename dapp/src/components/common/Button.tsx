d96d65dc78b3ad9666b3a6c74394897d117f2f31
Wasin Nimlaor <wasin.nimlaor@gmail.com>
2023-07-28 11:29:06 +0930

    Create common button component

---
+++ dapp/src/components/common/Button.tsx
@@ -0,0 +1,30 @@
+import { Button as BsButton, Spinner } from "react-bootstrap";
+
+interface ButtonProps {
+  onClick: () => void;
+  text: string;
+  loading?: boolean;
+}
+
+const Button: React.FC<ButtonProps> = ({ onClick, text, loading }) => {
+  return (
+    <BsButton onClick={onClick}>
+      {loading ? (
+        <>
+          <Spinner
+            as="span"
+            animation="border"
+            size="sm"
+            role="status"
+            aria-hidden="true"
+          />
+          <span className="mx-2">Loading...</span>
+        </>
+      ) : (
+        text
+      )}
+    </BsButton>
+  );
+};
+
+export default Button;
--- dapp/src/components/common/index.ts
+++ dapp/src/components/common/index.ts
@@ -1,1 +1,2 @@
+export { default as Button } from "./Button";
 export { ActionStatus, default as StatusLabel } from "./StatusLabel";
