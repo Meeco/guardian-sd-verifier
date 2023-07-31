71dde8fd95afb3865196cd8246cccc7082c4dbf5
Sunny <wnimlaor@nextfaze.com>
2023-07-31 10:40:58 +0930

    Update dApp metadata

--- dapp/src/bladeWeb3Service/initConnection.ts
+++ dapp/src/bladeWeb3Service/initConnection.ts
@@ -5,10 +5,13 @@ const initConnection = () => {
     ConnectorStrategy.WALLET_CONNECT, // preferred strategy is optional
     {
       // dApp metadata options are optional, but are highly recommended to use
-      name: "Selective Disclosure DApp",
-      description: "DApp description",
-      url: "https://selective-disclosure-dapp.io/",
-      icons: ["some-image-url.png"],
+      name: "Hedera Guardian Selective Disclosure",
+      description: "Hedera Guardian Selective Disclosure",
+      url: "https://hedera.com/guardian",
+      // TODO: upload icon for Hedera Guardian Selective Disclosure
+      icons: [
+        "https://api-sandbox.svx.exchange/blobs/1f345707-447f-4273-96bd-4c135e0286d8",
+      ],
     }
   );
 
