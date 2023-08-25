9ff18f72a5a72e90f45f5524394c2d45f6de7317
Sunny <wnimlaor@nextfaze.com>
2023-08-25 11:40:59 +0930

    Update env for dAap and server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,11 @@
-import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-// import { initSigningService } from "./signingService";
+import express from "express";
 
 const app = express();
 dotenv.config();
 
 const port = process.env.PORT || 8000;
-// const accountId = process.env.HEDERA_ACCOUNT_ID || "";
-// const privateKey = process.env.HEDERA_PRIVATE_KEY || "";
 
 app.use(cors());
 app.use(express.json());
@@ -17,13 +14,6 @@ app.use(express.urlencoded());
 app.listen(port, () => {
   console.log(`Server is running on port ${port}.`);
 });
-
-// initSigningService(accountId, privateKey);
-
-// app.get("/tx-bytes-string", async (req, res) => {
-//   const outBytesStr = await createTransactionString(privateKey, accountId);
-//   res.json({ outBytesStr });
-// });
 
 app.get("/resolve-did/:id", async (req, res) => {
   const { params } = req;
