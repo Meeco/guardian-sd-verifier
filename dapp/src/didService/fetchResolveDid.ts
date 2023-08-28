3ef16c944721b24a5e94b8638868b9025ca1a047
Sunny <wnimlaor@nextfaze.com>
2023-08-28 14:59:00 +0930

    remove unused server folder

--- dapp/src/didService/fetchResolveDid.ts
+++ dapp/src/didService/fetchResolveDid.ts
@@ -1,12 +1,23 @@
-const fetchResolveDid = async (did: string) => {
-  try {
-    const res = await fetch(
-      `${process.env.REACT_APP_VERIFIER_SERVER_URL}/resolve-did/${did}`
-    );
-    return await res.json();
-  } catch (error) {
-    console.log("Fetch resolve did failed", error);
-  }
-};
+import { fetchJson } from "../utils";
+
+const UNIVERSAL_RESOLVER_HOST =
+  process.env.UNIVERSAL_RESOLVER_HOST ??
+  `https://dev.uniresolver.io/1.0/identifiers`;
+const HEDERA_RESOLVER_HOST =
+  process.env.HEDERA_RESOLVER_HOST ?? `http://localhost:5000/1.0/identifiers`;
+
+/**
+ * Placeholder did resolver.
+ * Since universal resolvers don't yet typically support Hedera DIDs, assumes a
+ * universal did resolver running at localhost:5000 that will resolve Hedera DIDs
+ */
+const fetchResolveDid = (did: string) =>
+  fetchJson(
+    `${
+      did.startsWith("did:hedera")
+        ? HEDERA_RESOLVER_HOST
+        : UNIVERSAL_RESOLVER_HOST
+    }/${did}`
+  );
 
 export default fetchResolveDid;
