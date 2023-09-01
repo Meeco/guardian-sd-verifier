8444872139e5e5ee5f637ef84b91fa918cbe42f0
Sunny <wnimlaor@nextfaze.com>
2023-09-01 16:23:46 +0930

    move files from /dapp out and delete the folder

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# guardian-sd-verifier
-Guardian Selective Disclosure - Verifier App 
+# guardian-sd-veifier
+
+Guardian Selective Disclosure - Presentation Exchange Verifier DApp
+
+## Running
+
+Ensure `.env.example` is copied as `.env` and completed with all properties.
+
+It can be run locally with node > 16:
+
+- Install dependencies with `yarn` (`npm i -g yarn` to install yarn if you do not have it installed)
+- `yarn start` to start the responder listening on the configured topics
--- src/didService/fetchResolveDid.ts
+++ src/didService/fetchResolveDid.ts
@@ -1,15 +1,12 @@
 import { fetchJson } from "../utils";
 
-const UNIVERSAL_RESOLVER_HOST =
-  process.env.UNIVERSAL_RESOLVER_HOST ??
-  `https://dev.uniresolver.io/1.0/identifiers`;
 const HEDERA_RESOLVER_HOST =
   process.env.HEDERA_RESOLVER_HOST ?? `http://localhost:5000/1.0/identifiers`;
 
 /**
  * Placeholder did resolver.
  * Since universal resolvers don't yet typically support Hedera DIDs, assumes a
- * universal did resolver running at localhost:5000 that will resolve Hedera DIDs
+ * universal did resolver running at localhost:5000 that will resolve Hedera DIDs and other DIDs
  */
 const fetchResolveDid = (did: string) =>
   fetchJson(`${HEDERA_RESOLVER_HOST}/${did}`);
