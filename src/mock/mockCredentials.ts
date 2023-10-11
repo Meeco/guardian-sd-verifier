1f8c228a0aa610d07f36dd18c15e8a0e94af3521
Sunny <wnimlaor@nextfaze.com>
2023-10-11 10:56:09 +1030

    create deriveEdVerificationKey's tests

---
+++ src/mock/mockCredentials.ts
@@ -0,0 +1,18 @@
+export const mockId =
+  "did:key:z6MkuGB9nMVsZwJFR3WJwGBPsnjogYM5E4bvx4HFzsooFpRP#z6MkuGB9nMVsZwJFR3WJwGBPsnjogYM5E4bvx4HFzsooFpRP";
+export const mockDid =
+  "did:key:z6MknDUyDPK834QCtCVesmmacwFGhv8ukqbhoGao5kzzReDG";
+export const mockPrivateKeyHex =
+  "2110c05e8650e2c2a0564a472c8429b7b0ff102cf47267da0a96e4c5b825e3817355fa57dcff65fe79b8acee9d6edf7b05b80dc7ce0f34a0afa60985a2eda0ab";
+export const mockPublicKeyHex =
+  "7355fa57dcff65fe79b8acee9d6edf7b05b80dc7ce0f34a0afa60985a2eda0ab";
+
+export const mockEd25519VerificationKey2020Credentials = {
+  type: "Ed25519VerificationKey2020",
+  id: "did:hedera:testnet:z6MkmELdkLPDgwzwXSm166M2ut4i2M9GHALUiYvZBz15YzWG_0.0.1136547#did-root-key",
+  controller:
+    "did:hedera:testnet:z6MkmELdkLPDgwzwXSm166M2ut4i2M9GHALUiYvZBz15YzWG_0.0.1136547",
+  publicKeyMultibase: "z6MkmELdkLPDgwzwXSm166M2ut4i2M9GHALUiYvZBz15YzWG",
+  privateKeyMultibase:
+    "zrv1ZCXBBXsFAVF6gHkor79DhZ3fx3Nbo8GcgskR8DKHR1UyfKYe5gJubQQpNtRTA18HMsoPW1RW2LVKy8AB6SKFM9n",
+};
--- src/utils/createVerifiablePresentation.test.ts
+++ src/utils/createVerifiablePresentation.test.ts
@@ -1,4 +1,10 @@
 import mockCredential from "../mock/example_credential.json";
+import {
+  mockDid,
+  mockId,
+  mockPrivateKeyHex,
+  mockPublicKeyHex,
+} from "../mock/mockCredentials";
 import createSuite from "./createSuite";
 import createVerifiablePresentation from "./createVerifiablePresentation";
 import deriveEdVerificationKey from "./deriveEdVerificationKey";
@@ -7,12 +13,10 @@ import { documentLoader } from "./documentLoader";
 describe("createVerifiablePresentation", () => {
   it("should create Verifiable Presentation from Verifiable Credential", async () => {
     const verificationKey = await deriveEdVerificationKey({
-      id: "did:key:z6MkuGB9nMVsZwJFR3WJwGBPsnjogYM5E4bvx4HFzsooFpRP#z6MkuGB9nMVsZwJFR3WJwGBPsnjogYM5E4bvx4HFzsooFpRP",
-      did: "did:key:z6MknDUyDPK834QCtCVesmmacwFGhv8ukqbhoGao5kzzReDG",
-      privateKeyHex:
-        "2110c05e8650e2c2a0564a472c8429b7b0ff102cf47267da0a96e4c5b825e3817355fa57dcff65fe79b8acee9d6edf7b05b80dc7ce0f34a0afa60985a2eda0ab",
-      publicKeyHex:
-        "7355fa57dcff65fe79b8acee9d6edf7b05b80dc7ce0f34a0afa60985a2eda0ab",
+      id: mockId,
+      did: mockDid,
+      privateKeyHex: mockPrivateKeyHex,
+      publicKeyHex: mockPublicKeyHex,
       type: "Ed25519VerificationKey2018",
     });
 
