2454db067198315f6d7a048e76e4f856dda5cfb0
Zak Barbuto <zbarbuto@nextfaze.com>
2023-10-16 08:41:32 +1030

    Reduce time for test

--- src/utils/promiseWithTimeout.test.ts
+++ src/utils/promiseWithTimeout.test.ts
@@ -11,14 +11,18 @@ describe("promiseWithTimeout", () => {
   });
 
   it("should reject error if timeout", async () => {
+    let timeout;
     const promise = jest.fn().mockImplementation(() => {
-      return new Promise((resolve) => setTimeout(resolve, 6000, true));
+      return new Promise((resolve) => {
+        timeout = setTimeout(resolve, 600, true);
+      });
     }) as any;
 
     try {
-      await promiseWithTimeout({ promise: promise(), time: 1000 });
+      await promiseWithTimeout({ promise: promise(), time: 100 });
     } catch (error) {
       expect((error as any).message).toBe("Promise timed out");
     }
+    clearTimeout(timeout);
   });
 });
